e8aa219cea51fe991c44d9d8db675bd1aa7be542
Alexander Greer <alexanderkeithgreer@gmail.com>
2021-02-12 21:26:59 +1300

    Added pulse compression and LFM signals, starting to test basic parts of the simulation

--- mainMimoSim.py
+++ mainMimoSim.py
@@ -1,5 +1,8 @@
 import numpy as np
+import numpy.linalg as la
 import numpy.random as ra
+
+import signalLFM as lfm
 
 # Generic requirements:
 # For each Tx Antenna
@@ -15,8 +18,8 @@ def simulateTxToRx(tx_loc, target_loc, rx_loc, raw_waveform, rcs, fs, fc):
     """
 
     #d for distance
-    d_tx_t = norm(tx_loc - target_loc)
-    d_t_rx = norm(target_loc - rx_loc)
+    d_tx_t = la.norm(tx_loc - target_loc)
+    d_t_rx = la.norm(target_loc - rx_loc)
     d_total = d_tx_t + d_t_rx
     wavelength = 3e8/fc
 
@@ -27,19 +30,22 @@ def simulateTxToRx(tx_loc, target_loc, rx_loc, raw_waveform, rcs, fs, fc):
     loss = loss_rx_t * loss_tx_t * rcs
 
     output = np.zeros(len(raw_waveform))
-    output[n_delay:] raw_waveform * loss * np.exp(phase_shift)[:n_delay]
+    output[n_delay:] = raw_waveform * loss * np.exp(phase_shift)[:n_delay]
     return output;
 
-def simulate_whole_system(tx_locs, rx_locs, targets, waveforms, noise_power, interf=False):
+def simulateWholeSystem(tx_locs, target_locs, rx_locs, targets, waveforms, noise_power, fs, fc, interf=False):
     """
     tx_locs is a    n_tx * 3        matrix of tx antenna positions
+    tx_locs is a    n_targets * 3   matrix of target positions
     rx_locs is a    n_rx * 3        matrix of rx antenna positions
     targets is a    n_targets       vector of target rcs values
     waveforms is a  n_tx * n_samp   matrix of data
-    noise power defines the noise per Rx antenna (scalar)
-    interf will be used later to play with Tx antenna mutual coupling (bool)
-    """
+    noise power     defines the noise per Rx antenna (scalar)
+    interf will be  used later to play with Tx antenna mutual coupling (bool)
 
+    outputs is a    n_rx *n_samp    matrix of received signals
+
+    """
     n_tx = len(tx_locs[:,0])
     n_rx = len(rx_locs[:,0])
     n_waveforms = len(waveforms[:,0])
@@ -54,6 +60,35 @@ def simulate_whole_system(tx_locs, rx_locs, targets, waveforms, noise_power, int
     for rx in np.arange(n_rx):
         for tx in np.arange(n_tx):
             for target in np.arange(n_targets):
-                outputs[rx,:] = simulateTxToRx(tx_locs[tx], rx_locs[rx], targets[target], waveforms[target])
+                outputs[rx,:] = simulateTxToRx(tx_locs[tx], target_locs[target],
+                    rx_locs[rx], waveforms[tx], targets[target], fs, fc)
 
     outputs += ra.randn(n_rx, n_samp) * noise_power**(1/2)
+
+    return outputs
+
+
+def simulateWholeSystem_test():
+    fc = 2.4e9
+    wl = 3e8/fc
+    t_duration = 1e-3
+    T = 1e-3
+    Tp = 2e-4
+    B = 3e6
+    fs = 10e6
+    A0 = 1
+    rcs = np.array([[1]])
+
+    antennas_tx = np.array(([[wl*0.25,0,0],[-wl*0.25,0,0]]))
+    target = np.array([[0,10000,0]])
+    antennas_rx = np.array([[0,0,0],[wl*0.5,0,0],[-wl*0.5,0,0]])
+
+    pulse_low, ref_low = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 0, 10e6, 1 )
+    pulse_high, ref_high = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 5e6, 10e6, 1 )
+
+    waveforms = np.array([pulse_low, pulse_high])
+    raw_outputs = simulateWholeSystem(antennas_tx, target, antennas_rx, rcs, waveforms, 0.0, fs, fc)
+
+    plt.plot(10*np.log10(np.abs(nfft.fft(raw_outputs[0]))))
+
+simulateWholeSystem_test()
--- signalLFM.py
+++ signalLFM.py
@@ -1,4 +1,5 @@
 import numpy as np
+import numpy.random as ra
 import matplotlib.pyplot as plt
 import numpy.fft as nfft
 
@@ -49,11 +50,6 @@ def generateChirpPulsedLFM(t ,T, Tp, B, fc, fs, A0, verbose = False):
         if (n_samp <= reference_end):
             pulse_end = (reference_end - 1)
 
-        print(pulse_start)
-        print(pulse_end)
-        print(pulsedLFM[pulse_start:pulse_end])
-        print(chirp[0:n_samp_pulse_on])
-
         pulsedLFM[pulse_start:pulse_end] = chirp[0:(n_samp_pulse_on-1)]
         referenceLFM[pulse_start:reference_end] = chirp[0:(n_samp_ref_on-1)]
 
@@ -70,4 +66,100 @@ def generateChirpPulsedLFM_test():
 
     plt.plot(10*np.log10(np.abs(nfft.fft(mix)+1e-20)), label="mix")
     plt.plot(10*np.log10(np.abs(nfft.fft(tx)+1e-20)), label="tx")
+    plt.show()
+
+
+def processLFMFourier(reference, received):
+    """
+    reference is a   n_samp       vector that is the reference radar signal
+    received  is a   n_samp       vector that is the received radar signal
+    """
+
+    output_time = reference * received
+    output = nfft.fft(output_time)
+    return output
+
+def processLFMPulseCompression(reference, received, fs, f_max, r_max):
+    """
+    Implements pulse compression using the fourier method.
+    Remember with delays that n = fs * r/c; s^(-1) * m / ms^(-1)
+
+    reference is a      n_samp   length vector
+
+    """
+
+    #Constants
+    c = 3e8
+    n_samples_ref = len(reference)
+    n_samples_rec = len(received)
+
+    delay_max = np.int64(np.round(fs * r_max/c))
+
+    f_step = fs / (n_samples_ref) #Presumably set to this value because of the limits imposed by the CRB
+    print("f_step = ", f_step)
+    n_freq_step = np.int64(np.round( f_max/f_step ))
+
+    freq_shifts = np.arange(-n_freq_step, n_freq_step + 1, 1)
+
+    output = np.zeros((delay_max, 2 * n_freq_step + 1), dtype = np.complex128)
+
+    reference_f = nfft.fft(reference)
+    received_f = nfft.fft(received)
+
+    for shift in freq_shifts:
+        #Multiply (in frequency domain, time domain convolution)
+        output_f = np.conj(np.roll(reference_f,shift)) * received_f
+        #Convert back to time domain and add to the output vector:
+        output[:,shift + n_freq_step] = nfft.ifft(output_f)[:delay_max]
+
+    return output
+
+
+def processLFMPulseCompression_test():
+    """
+    Exists to test the pulse compression. Specifically:
+    If correlation produces a peak
+    If it doesn't product a peak for uncorr signals
+    If the delay works
+    If the phase extraction works
+    If the frequency shift works.
+
+    No automatic testing, based purely on visual inspection.
+    """
+
+    awgn = ra.randn(100000) + 1j*ra.randn(100000)
+    awgn_roll = np.roll(awgn, 12) #Delay of 12 corresponds to 30*12 = 360m?
+    awgn_phase = np.exp(1j*1.345)*awgn
+    awgn_uncorr = ra.randn(100000) + 1j*ra.randn(100000)
+    f_shift = 40
+    f_s = 2e6   #n_samp = 100e3, fs=2e6 gives a doppler
+                #   step of 20Hz in the fourier domain
+    awgn_doppler = awgn * np.exp(2j*np.pi*(f_shift/f_s)*np.arange(100000))
+
+    plt.plot(10*np.log10(np.abs(nfft.fft(awgn))))
+    plt.plot(10*np.log10(np.abs(nfft.fft(awgn_doppler))))
+    plt.show()
+
+    output_same = processLFMPulseCompression(awgn, awgn, f_s, 500, 1e3)
+    output_roll = processLFMPulseCompression(awgn, awgn_roll, f_s, 500, 1e3)
+    output_uncorr = processLFMPulseCompression(awgn, awgn_uncorr, f_s, 500, 1e3)
+    output_phase = processLFMPulseCompression(awgn, awgn_phase, f_s, 500, 1e3)
+    output_doppler = processLFMPulseCompression(awgn, awgn_doppler, f_s, 500, 1e3)
+
+    plt.figure()
+    plt.imshow(10*np.log10(np.abs(output_same)))
+    plt.title("Same")
+    plt.figure()
+    plt.imshow(10*np.log10(np.abs(output_roll)))
+    plt.title("Delay")
+    plt.figure()
+    plt.imshow(10*np.log10(np.abs(output_uncorr)))
+    plt.title("Uncorr")
+    plt.figure()
+    plt.imshow(10*np.log10(np.abs(output_doppler)))
+    plt.title("Dopp")
+    plt.figure()
+    plt.imshow(np.angle(output_phase))
+    plt.title("Phase")
+
     plt.show()
