dfcca3232c5e6c5e38c2f29044336b4f4a2ec740
AlexanderKeithGreer <AlexanderKeithGreer@gmail.com>
2022-11-23 22:01:26 +1300

    Reworked mimo beamforming to work robustly...will come back to clean up at a later date

--- main.py
+++ main.py
@@ -9,7 +9,7 @@ import signalLFM as lfm
 import mainMimoProcessing as pro
 import mainMimoSim as sim
 
-def mainRawSeeringVectors():
+def mainRawSteeringVectors():
     """
     Places a target at some distance, and attempts to use naive steering
         vectors to extract information, ie is non adaptive. The purpose of this
@@ -58,7 +58,7 @@ def mainRawSeeringVectors():
     y_two, peak_two = pro.extractPeakRangeDoppler(outputs_two, waveform_two, fs, 10, 400)
     y_four, peak_four = pro.extractPeakRangeDoppler(outputs_four, waveform_four, fs, 10, 400)
 
-    angles = np.deg2rad(np.arange(0,180))
+    angles = np.deg2rad(np.arange(-90,270))
 
     results_solo = np.zeros(len(angles))
     results_two = np.zeros(len(angles))
@@ -109,56 +109,99 @@ def mainSuperdirectiveSNR():
     B = 2.5e6
     fs = 10e6
     A0 = 1
+    sc = 0.5
+
     rcs = np.array([[1]])
-    target = np.array([[0,100,0]])
+    targetTheta = 90*np.pi/180
+    target = np.array([[1000*np.cos(targetTheta),1000*np.sin(targetTheta),0]])
+    print("Define Radar target at ", target)
+
+    plt.show()
+
     phi = 0
+    angles = np.deg2rad(np.arange(-180,180,1))
+    targ = np.zeros(len(angles))
+    for tt in range(len(targ)):
+        if (abs(targetTheta - angles[tt]) < 1e-4):
+            targ[tt] = 1
 
     pulse_0, ref_0 = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 0, fs, A0 )
     pulse_1, ref_1 = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 2.5e6, fs, A0 )
-    pulse_2, ref_2 = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 5e6, fs, A0 )
+    pulse_2, ref_2 = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 5.0e6, fs, A0 )
     pulse_3, ref_3 = lfm.generateChirpPulsedLFM(t_duration, T, Tp, B, 7.5e6, fs, A0 )
 
-    rx_five = np.array([[2*hl,0,0],[1*hl,0,0],[0,0,0],[-1*hl,0,0],[-2*hl,0,0]])
-    tx_four = np.array([[4.5*hl,0,0],[2.5*hl,0,0],[-2.5*hl,0,0],[-4.5*hl,0,0]])
+    rx_five = np.array([[8.0,0,0],[4.0,0,0],[0,0,0],[-4.0,0,0],[-8.0,0,0]])*hl
+    rx_one = np.array([[0,0,0]])
+    rx_cross_four = np.array([[0,2.5,0],[0,1.5,0],[0,0.5,0],[0,-0.5,0],[0,-1.5,0],[0,-2.5,0]])*hl
+    tx_four = np.array([[0.75,0,0],[0.25,0,0],[-0.25,0,0],[-0.75,0,0]])*wl
 
-    rx_tx_virtual = np.kron(tx_four[:,0], rx_five[:,0])
+    rx_use = rx_five
+
+    rx_tx_virtual = pro.generateVirtualArray(tx_four, rx_use)
+
+    plt.figure()
+    #plt.scatter(target[:,0], target[:,1])
+    plt.scatter(tx_four[:,0], tx_four[:,1])
+    plt.scatter(rx_use[:,0], rx_use[:,1])
+    plt.figure()
+    #plt.scatter(target[:,0], target[:,1])
+    plt.scatter(rx_tx_virtual[:,0], rx_tx_virtual[:,1])
+    plt.figure()
+    plt.scatter(target[:,0], target[:,1])
+    plt.scatter(rx_tx_virtual[:,0], rx_tx_virtual[:,1])
+
 
     waveform_four = np.array([pulse_0, pulse_1, pulse_2, pulse_3])
-    outputs_four = sim.simulateWholeSystem(tx_four, target, rx_five, rcs, waveform_four, 0.0, fs, fc)
+    outputs_four = sim.simulateWholeSystem(tx_four, target, rx_use, rcs, waveform_four, 0.0, fs, fc)
     y_four, peak_four = pro.extractPeakRangeDoppler(outputs_four, waveform_four, fs, 10, 400)
-    angles = np.deg2rad(np.arange(0,180,0.25))
 
     results_sv = np.zeros(len(angles))
     results_10 = np.zeros(len(angles))
     results_15 = np.zeros(len(angles))
     results_20 = np.zeros(len(angles))
 
-    R = pro.generateCovarianceIsotropic(tx_four, rx_five, fc)
+    R = pro.generateCovarianceIsoMimo(tx_four, rx_use, fc)
+    print(R)
+    #R = np.identity(np.shape(R)[0])
 
     for angle in range(len(angles)):
-        s_sv = pro.generateMimoSteeringVector(angles[angle], phi, tx_four, rx_five, fc)
-        s_10 = pro.generateSuperdirective(R, 10, angles[angle], phi, tx_four, rx_five, fc)
-        s_15 = pro.generateSuperdirective(R, 16, angles[angle], phi, tx_four, rx_five, fc)
-        s_20 = pro.generateSuperdirective(R, 20, angles[angle], phi, tx_four, rx_five, fc)
+        s_sv = pro.generateMimoSteeringVector(angles[angle], phi, tx_four, rx_use, fc)
+        s_10 = pro.generateSuperdirectiveKron(10, angles[angle], phi, tx_four, rx_use, fc)
+        s_15 = pro.generateSuperdirectiveKron(20, angles[angle], phi, tx_four, rx_use, fc)
+        s_20 = pro.generateSuperdirectiveKron(40, angles[angle], phi, tx_four, rx_use, fc)
 
         results_sv[angle] = (np.abs(np.inner(s_sv,y_four)))
         results_10[angle]  = (np.abs(np.inner(s_10,y_four)))
         results_15[angle] = (np.abs(np.inner(s_15,y_four)))
         results_20[angle] = (np.abs(np.inner(s_20,y_four)))
 
+    #plt.figure()
+    #plt.plot(np.rad2deg(angles), 20*np.log10(results_10/max(results_10)), label = "10")
+    #plt.plot(np.rad2deg(angles), 20*np.log10(results_sv/max(results_sv)), label = "sv")
+    #plt.plot(np.rad2deg(angles), 20*np.log10(targ+1e-2), label = "Targ")
+    #plt.legend()
+    #plt.xlabel("Angle (Degrees)")
+    #plt.ylabel("Power")
+
+    #plt.figure()
+    #plt.plot(np.rad2deg(angles), 20*np.log10(results_10/max(results_10)), label = "10")
+    #plt.plot(np.rad2deg(angles), 20*np.log10(results_20/max(results_20)), label = "20", #color="green")
+    #plt.plot(np.rad2deg(angles), 20*np.log10(targ+1e-2), label = "Targ")
+    #plt.legend()
+    #plt.xlabel("Angle (Degrees)")
+    #plt.ylabel("Power")
+
     plt.figure()
-    plt.plot(np.rad2deg(angles), 20*np.log10(results_10/max(results_10)), label = "10")
+    plt.plot(np.rad2deg(angles), 20*np.log10(results_10/max(results_10)), label = "Cov + Ie-10")
+    plt.plot(np.rad2deg(angles), 20*np.log10(results_15/max(results_15)), label = "Cov + Ie-20")
+    plt.plot(np.rad2deg(angles), 20*np.log10(results_20/max(results_20)), label = "Cov + Ie-40")
     plt.plot(np.rad2deg(angles), 20*np.log10(results_sv/max(results_sv)), label = "sv")
+    plt.plot(np.rad2deg(angles), 20*np.log10(targ+1e-2), label = "Targ")
     plt.legend()
     plt.xlabel("Angle (Degrees)")
     plt.ylabel("Power")
 
-    plt.figure()
-    plt.plot(np.rad2deg(angles), 20*np.log10(results_10/max(results_10)), label = "10")
-    plt.plot(np.rad2deg(angles), 20*np.log10(results_20/max(results_20)), label = "20", color="green")
-    plt.legend()
-    plt.xlabel("Angle (Degrees)")
-    plt.ylabel("Power")
     plt.show()
+
 
 mainSuperdirectiveSNR()
--- mainMimoProcessing.py
+++ mainMimoProcessing.py
@@ -28,7 +28,7 @@ def generateSteeringVector(theta, phi, array, fc):
     s = np.zeros((n_elem,1), dtype=np.complex128)
     k = np.array([np.cos(theta)*np.cos(phi),np.sin(theta)*np.cos(phi),np.sin(phi)])
     for elem in range(n_elem):
-        s[elem] = np.exp(-2j*np.pi*np.dot((fc/c)*array[elem,:],k))
+        s[elem] = np.exp(2j*np.pi*np.dot((fc/c)*array[elem,:],k))
 
     return s
 
@@ -52,6 +52,61 @@ def generateMimoSteeringVector(theta, phi, array_tx, array_rx, fc):
     s = np.kron(s_tx, s_rx).flatten()
 
     return s
+
+
+def generateMimoSteeringVector_test():
+    """
+    Visual verification of the generateMimoSteeringVector
+     function. Intended to ensure that those vectors
+    """
+    fc = 2.4e9 #2.4GHz
+    c = 3e8 #I hope this hasn't changed much
+    hl = fc*c /2
+    array_tx = np.array([[hl*0.25,0,0],[hl*0.25,0,0]])
+    array_tx = np.array([[hl*0.25,0,0],[hl*0.25,0,0]])
+
+def generateVirtualArray(array_tx, array_rx):
+    """
+    INPUTS:
+    array_tx is a      n_tx * 3      matrix of antenna locations
+    array_rx is a      n_rx * 3      matrix of antenna locations
+
+    OUTPUTS:
+    s is a             n_tx*n_rx    matrix of virtual antenna locations
+    """
+    n_tx = np.shape(array_tx)[0]
+    n_rx = np.shape(array_rx)[0]
+    array_vl = np.zeros([n_tx*n_rx,3])
+
+    for tx in range(n_tx):
+        for rx in range(n_rx):
+            array_vl[tx*n_rx+rx-1,:] = array_tx[tx,:] + array_rx[rx,:]
+    return array_vl
+
+def generateVirtualArray_test():
+    """ """
+    fc = 2.4e9
+    wl = 3e8/fc
+    hl = wl/2 #Half wavelength. Short definition to minimise code line length
+
+    rx_five = np.array([[3.0*hl,0,0],[1.5*hl,0,0],[0,0,0],[-1.5*hl,0,0],[-3.0*hl,0,0]])
+    rx_one = np.array([[0,0,0],[2*hl,0,0]])
+    tx_four = np.array([[0.75*hl,0,0],[0.25*hl,0,0],[-0.25*hl,0,0],[-0.75*hl,0,0]])
+    targetTheta = 90*np.pi/180
+    target = np.array([[1000*np.cos(targetTheta),1000*np.sin(targetTheta),0]])
+
+
+    rx_tx_virtual = generateVirtualArray(tx_four, rx_one)
+
+    plt.figure()
+    plt.scatter(target[:,0], target[:,1])
+    plt.scatter(tx_four[:,0], tx_four[:,1])
+    plt.scatter(rx_one[:,0], rx_one[:,1])
+    plt.figure()
+    plt.scatter(target[:,0], target[:,1])
+    plt.scatter(rx_tx_virtual[:,0], rx_tx_virtual[:,1])
+    plt.show()
+
 
 def extractPeakRangeDoppler(raw_waveforms, ref_waveforms, fs, f_max, r_max):
     """
@@ -82,11 +137,75 @@ def extractPeakRangeDoppler(raw_waveforms, ref_waveforms, fs, f_max, r_max):
 
     return y, peak
 
-def generateCovarianceIsotropic(array_tx, array_rx, fc):
+def sincComplex(inputNo):
+    """
+    Equivalent to sinc function in accoustic SDB, but works with complex numbers
+      to give elements in a coherence matrix. Complex part is the Hilbert
+      transform of a sinc,  (1-cos(x))/x.
+    Do remember that numpy version of sinc is sin(pi*x)/(pi*x)
+
+    INPUTS:
+      inputNo is a    scalar      real number
+    OUTPUTS:
+      output is a   scalar      real number
+    """
+    if (inputNo != 0):
+        output = np.sinc(inputNo) + 1j*(1-np.cos(np.pi*inputNo))/(np.pi*inputNo)
+    else:
+        output = 1
+    return output
+
+def sincComplex_test():
+    """
+    Relatively quick check to ensure that the function defined works as expected
+    """
+    x = np.arange(-10,10,0.01)
+    y = np.zeros(len(x), dtype=np.complex128)
+    for ii in range(len(x)):
+        y[ii] = sincComplex(x[ii])
+
+    plt.plot(x,np.angle(y), label="arg")
+    plt.plot(x,10*np.log10(np.real(y)+1e-10),label="R")
+    plt.plot(x,10*np.log10(np.imag(y)+1e-10),label="I")
+    plt.plot(x,10*np.log10(np.abs(y)+1e-10),label="Mag")
+    plt.legend()
+    plt.show()
+
+def generateCovarianceIso(array, fc):
+    """
+    Isotropic noise model is based on Ch 2 of Brandstein's Microphone Array
+      Signal processing.
+
+    INPUTS:
+    array_tx is a   n_elem x 3      matrix of tx antenna locations
+    fc is a         scalar          parameter for the carrier frequency
+
+    OUTPUTS:
+    R is a          n_elem x n_elem   matrix (covariance matrix)
+    """
+    n_elem = np.shape(array)[0]       #number of virtual elements
+    c = 3e8
+
+    R = np.zeros([n_elem, n_elem],dtype=np.complex128)
+    l = np.zeros([n_elem, n_elem],dtype=np.complex128)
+
+    #This is a block matrix -- it can -probably- be kroned together
+    for N in range(n_elem):
+        for M in range(n_elem):
+            l[M,N] = (fc/c)*la.norm(array[M,:] - array[N,:])
+            R[M,N] = np.sinc((fc/c)*la.norm(array[M,:] - array[N,:]))
+
+    return R
+
+
+
+def generateCovarianceIsoMimo(array_tx, array_rx, fc):
     """
     Assumes (mostly co-located arrays). TX is the major dimension.
     Isotropic noise model is based on Ch 2 of Brandstein's Microphone Array
       Signal processing.
+    It is based on generating the covariance matrices associated with
+      the TX and RX and kron'ing them together
 
     INPUTS:
     array_tx is a   n_elem_tx x 3   matrix of tx antenna locations
@@ -97,27 +216,17 @@ def generateCovarianceIsotropic(array_tx, array_rx, fc):
     R is a          (n_elem_tx*n_elem_rx)   matrix (covariance matrix)
                     x (n_elem_tx*n_elem_rx)
     """
-    n_tx = np.shape(array_tx)[0]
-    n_rx = np.shape(array_rx)[0]
-    n_elem = n_tx * n_rx         #number of virtual elements
-    c = 3e8
+    R_tx = generateCovarianceIso(array_tx, fc)
+    R_rx = generateCovarianceIso(array_rx, fc)
 
-    R_tx = np.zeros([n_tx, n_tx])
-    R_rx = np.zeros([n_rx, n_rx])
-    #This is a block matrix -- it can -probably- be kroned together
-    for N in range(n_tx):
-        for M in range(n_tx):
-            R_tx[M,N] = np.sinc(2*np.pi*np.dot((fc/c)*array_tx[M,:], array_tx[N,:]))
-    for N in range(n_rx):
-        for M in range(n_rx):
-            R_rx[M,N] = np.sinc(2*np.pi*np.dot((fc/c)*array_rx[M,:], array_rx[N,:]))
     R = np.kron(R_tx, R_rx)
     return R
 
-def generateSuperdirective(R, SNR, theta, phi, array_tx, array_rx, fc):
+def generateSuperdirectiveVirtual(SNR, theta, phi, array_tx, array_rx, fc):
     """
     Assumes (mostly) co-located arrays. TX is the major dimension.
-    Solves the MVDR using isotropic
+    Solves the MVDR using the covariance matrix assoc with an
+    isotropic noise field.
 
     INPUTS:
     array_tx is a   n_elem_tx x 3           matrix of tx antenna locations
@@ -137,11 +246,13 @@ def generateSuperdirective(R, SNR, theta, phi, array_tx, array_rx, fc):
     n_rx = len(array_rx)
     n_elem = len(array_tx) * len(array_rx)
     a = generateMimoSteeringVector(theta, phi, array_tx, array_rx, fc)
-    R = generateCovarianceIsotropic(array_tx, array_rx, fc)
+    array_vl = generateVirtualArray(array_tx, array_rx)
+
+    R = generateCovarianceIso(array_vl, fc)
     R += np.identity(n_elem) * (1/10**(SNR/10))
     R_inv = la.inv(R)
     Ra = np.matmul(R_inv, np.array([a]).T) #Not a Hermitian! Just need to covert to 2d
-    s = Ra/np.matmul(Ra.T.conj(), a)
+    s = Ra/np.matmul(a.T.conj(), Ra)
     s = s.flatten()
     return s
 
@@ -157,9 +268,92 @@ def generateSuperdirective_test():
     rx_five = np.array([[2*hl,0,0],[hl,0,0],[0,0,0],[-hl,0,0],[-2*hl,0,0]])
     tx_two  = np.array([[2.5*hl,0,0],[-2.5*hl,0,0]])
     tx_four = np.array([[5*hl,0,0],[2.5*hl,0,0],[-2.5*hl,0,0],[-5*hl,0,0]])
-    R_two   = generateCovarianceIsotropic(tx_two,  rx_five, fc)
-    s_two   = generateSuperdirective(R_two, tx_two,  rx_five, fc, theta, phi, 20)
-    R_four  = generateCovarianceIsotropic(tx_four, rx_five, fc)
-    s_four  = generateSuperdirective(R_two, tx_four, rx_five, fc, theta, phi, 20)
+    R_two   = generateCovarianceIsoMimo(tx_two,  rx_five, fc)
+    s_two   = generateSuperdirective(R_two, 20, theta, phi, tx_two, rx_five, fc)
+    R_four  = generateCovarianceIsoMimo(tx_four, rx_five, fc)
+    s_four  = generateSuperdirective(R_four, 20, theta, phi, tx_four, rx_five, fc)
+    plt.figure()
+    plt.imshow(np.angle(R_four))
+    plt.figure()
+    plt.imshow(np.abs(R_four))
+    plt.show()
+    print(s_two)
+    print(s_four)
+
+def generateSuperdirectiveKron(SNR, theta, phi, array_tx, array_rx, fc):
+    """
+    Assumes (mostly) co-located arrays. TX is the major dimension.
+    Solves the MVDR using the covariance matrix assoc with an
+    isotropic noise field.
+
+    INPUTS:
+    array_tx is a   n_elem_tx x 3           matrix of tx antenna locations
+    array_rx is a   n_elem_rx x 3           matrix of rx antenna locations
+    R is a          (n_elem_tx*n_elem_rx)   matrix (covariance matrix)
+                    x (n_elem_tx*n_elem_rx)
+    fc is a         scalar                  carrier frequency
+    theta is a      scalar                  parameter to define azimuth
+    phi is a        scalar                  parameter to define altitude
+    SNR is a        scalar                  parameter used for robustness (dB)
+
+
+    OUTPUTS:
+    s is a          n_elem_tx * n_elem_rx   mimo steering vector
+    """
+    n_tx = np.shape(array_tx)[0]
+    n_rx = np.shape(array_rx)[0]
+    s_tx = generateSuperdirective(0, SNR, theta, phi, array_tx, fc)
+    s_rx = generateSuperdirective(0, SNR, theta, phi, array_rx, fc)
+    s = np.kron(s_tx, s_rx).flatten()
+    return s
+
+def generateSuperdirective(R, SNR, theta, phi, array, fc):
+    """
+    Solves the MVDR using the covariance matrix assoc with an
+    isotropic noise field.
+
+    INPUTS:
+    array is a      n_elem x    3           matrix of tx antenna locations
+    R is a          (n_elem x n_elem)       matrix (covariance matrix)
+    fc is a         scalar                  carrier frequency
+    theta is a      scalar                  parameter to define azimuth
+    phi is a        scalar                  parameter to define altitude
+    SNR is a        scalar                  parameter used for robustness (dB)
+
+
+    OUTPUTS:
+    s is a          n_elem                  mimo steering vector
+    """
+    n_elem = len(array)
+    a = generateSteeringVector(theta, phi, array, fc)
+    R = generateCovarianceIso(array, fc)
+    R += np.identity(n_elem) * (1/10**(SNR/10))
+    R_inv = la.inv(R)
+    Ra = np.matmul(R_inv, a) #Not a Hermitian! Just need to covert to 2d
+    s = Ra/np.matmul(a.T.conj(), Ra)
+    s = s.flatten()
+    return s
+
+def generateSuperdirectiveKron_test():
+    """
+    Quickly check the geometry and coefficient values
+    """
+    theta = 35
+    phi = 0
+    fc = 2.4e9
+    wl = 3e8/fc
+    hl = wl/2 #Half wavelength. Short definition to minimise code line length
+    rx_five = np.array([[2*hl,0,0],[hl,0,0],[0,0,0],[-hl,0,0],[-2*hl,0,0]])
+    tx_two  = np.array([[2.5*hl,0,0],[-2.5*hl,0,0]])
+    tx_four = np.array([[5*hl,0,0],[2.5*hl,0,0],[-2.5*hl,0,0],[-5*hl,0,0]])
+    R_two   = generateCovarianceIsoMimo(tx_two,  rx_five, fc)
+    s_two   = generateSuperdirective(R_two, 20, theta, phi, tx_two, rx_five, fc)
+    R_four  = generateCovarianceIsoMimo(tx_four, rx_five, fc)
+    s_four  = generateSuperdirective(R_four, 20, theta, phi, tx_four, rx_five, fc)
+    plt.figure()
+    plt.imshow(np.angle(R_four))
+    plt.figure()
+    plt.imshow(np.abs(R_four))
+    plt.show()
     print(s_two)
     print(s_four)
